7cb1426d96300bb3cba4ae5e6d75b4628f5f0288
Geetanshu <149077375+Geetanshu0410@users.noreply.github.com>
2024-09-01 23:59:30 +0530

    Update array.cpp

--- LEETCODE/array.cpp
+++ LEETCODE/array.cpp
@@ -105,3 +105,30 @@ public:
 //             return k;
 //     }
 // };
+
+
+ (Q37)
+//  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+ class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int size =nums.size();
+        int s=0;
+        int e=size-1;
+        while(e>=s){
+            int mid=(s+e)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if (target<nums[mid]){
+                e=mid-1;
+            }
+            else if (target>nums[mid]){
+                s=mid+1;
+            }
+        }
+return s ;
+}
+};

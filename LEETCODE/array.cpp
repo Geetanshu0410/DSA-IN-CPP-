25642a707b70ea303580d864f224fe0819124a10
Geetanshu <149077375+Geetanshu0410@users.noreply.github.com>
2024-09-02 00:00:59 +0530

    Update array.cpp

--- LEETCODE/array.cpp
+++ LEETCODE/array.cpp
@@ -107,7 +107,7 @@
 // };
 
 
- (Q37)
+ (Q35)
 //  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 // You must write an algorithm with O(log n) runtime complexity.

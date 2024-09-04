2ad1941a8be236c7ffd85f42b84e6c9dd3429bb3
Geetanshu <149077375+Geetanshu0410@users.noreply.github.com>
2024-09-04 22:48:21 +0530

    Update array.cpp

--- LEETCODE/array.cpp
+++ LEETCODE/array.cpp
@@ -157,3 +157,27 @@
 //         return digits;
 //         }
 // };
+
+ Q852
+ // You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+// Return the index of the peak element.
+
+// Your task is to solve it in O(log(n)) time complexity.
+//  class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int s=0;
+//         int e=arr.size()-1;
+      
+//         while(e>s){
+//               int mid=s+(e-s)/2;
+//             if(arr[mid]<arr[mid+1]){
+//                 s=mid+1;
+//             }
+//             else{
+//                 e=mid;
+//             }
+//         }
+//     return s;}
+// };

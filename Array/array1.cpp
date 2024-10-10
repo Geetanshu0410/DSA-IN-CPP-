4b62e7653de66ae1499cf7256b4b83176eec25bd
Geetanshu <149077375+Geetanshu0410@users.noreply.github.com>
2024-10-10 21:47:49 +0530

    Update array1.cpp

--- Array/array1.cpp
+++ Array/array1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //                               creating the function for the linear search// 
 //(1)   
-    //  int findingtarget(int arr[],int size  ,int targert){ // creating a fucntion with its requirement 
+    //  int findingtarget(int arr[],int size  ,int targert){ // creating a fucntion wi  th its requirement 
     //     for (int i = 0; i < size; i++) // tellling it    size to so that loop can be run for that time 
     //     {
     //         if (arr[i]==targert)              // this statement will rectify this 
